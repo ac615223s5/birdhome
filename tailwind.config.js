411ed882b5119e09568e578825d38455f8983372
ac615223s5 <ppu7haxj@duck.com>
2025-01-07 21:16:54 +0000

    Deploying to gh-pages from @ ac615223s5/birdhome@583a539e4dfb91e0fb5edd4207cd3a785fc357fe 🚀

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: [
+    './collections/_drafts/**/*.{html,md,markdown}',
+    './collections/_posts/**/*.{html,md,markdown}',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './pages/*.{html,md,markdown}',
+    './*.{html,md,markdown}',
+  ],
+  safelist:[
+    'grid-cols-1',
+    'grid-cols-2',
+    'grid-cols-3',
+    'grid-cols-4',
+    'grid-cols-5',
+    'grid-cols-6',
+    'dark:bg-zinc-950',
+    'dark:bg-zinc-900',
+    'dark:bg-black',
+    '!dark:border-0',
+  ],
+  theme: {
+    theme: {
+      extend: {},
+    },
+  },
+  corePlugins: {
+    
+  },
+  plugins: []
+}
